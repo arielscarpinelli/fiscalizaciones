25f46b28c4d013705e5ed60c72bec73c5257c69a
Ariel Scarpinelli <ascarpinelli@spredfast.com>
2021-09-07 17:14:27 -0300

    oops

--- backend/src/migrations/20210907154500-update-listas.js
+++ backend/src/migrations/20210907154500-update-listas.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.remove('Listas', 'listas');
+    await queryInterface.removeColumn('Listas', 'listas');
     await queryInterface.addColumn('Listas', 'lista', { type: Sequelize.STRING });
     await queryInterface.addColumn('Listas', 'cargos', { type: Sequelize.STRING });
     await queryInterface.removeIndex('Listas', 'listas_eleccion');
