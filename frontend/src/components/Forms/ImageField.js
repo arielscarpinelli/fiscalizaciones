c117c41fb21fa9217d48a329b3e3e6278500338e
Ariel Scarpinelli <ascarpinelli@spredfast.com>
2021-11-01 19:28:05 -0300

    zoom y rotacion de imagen

--- frontend/src/components/Forms/ImageField.js
+++ frontend/src/components/Forms/ImageField.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import {Controller, useFormContext, useWatch} from "react-hook-form";
+import ImageMagnifier from "components/Forms/ImageMagnifier";
 
 const URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
@@ -17,6 +18,10 @@ const ImageField = ({name, label, readOnly, ...rest}) => {
       ? URL.createObjectURL(current[0])
       : null)
 
+  const [rotate, doSetRotate] = useState(0);
+
+  const setRotate = angle => doSetRotate(((angle < 0) ? (angle + 360) : angle) % 360)
+
   return (
     <Controller
       defaultValue={null}
@@ -28,25 +33,34 @@ const ImageField = ({name, label, readOnly, ...rest}) => {
                e.dataTransfer.files.length && onChange(e.dataTransfer.files);
                e.preventDefault();
              }}>
-          {img && <img
+          {img && <div style={{overflow: "hidden"}}>
+            <ImageMagnifier
               src={img}
               className="d-block foto"
               alt={label}
+              style={{
+                transform: `rotate(${rotate}deg)`,
+              }}
               onLoad={(e) => typeof current !== "string" && URL.revokeObjectURL(e.target.src)}
-          />}
+            />
+          </div>}
+          {img && <div className="btn-group" role="group">
+            <button type="button" className="btn btn-sm btn-secondary" onClick={() => setRotate(rotate - 90)}>Rotar izquierda</button>
+            <button type="button" className="btn btn-sm btn-secondary" onClick={() => setRotate(rotate + 90)}>Rotar derecha</button>
+          </div>}
           {!readOnly && (
-            <label className="btn btn-sm btn-secondary">
+            <div><label className="btn btn-sm btn-secondary">
               <input
                 type="file"
                 accept="image/*"
                 name={name}
                 ref={ref}
                 hidden
-                onChange={e => e.target.files.length && onChange(e.target.files)}
+                onChange={e => {e.target.files.length && onChange(e.target.files); setRotate(0)}}
                 onBlur={onBlur}
               />
               {img ? "Cambiar" : "Subir"} {label}
-            </label>
+            </label></div>
           )}
           {error && <div><small className="text-danger">{error}</small></div>}
         </div>)}/>
---
+++ frontend/src/components/Forms/ImageMagnifier.js
@@ -0,0 +1,85 @@
+import React, {useState} from "react";
+
+export default function ImageMagnifier({
+                                         src,
+                                         magnifierHeight = 150,
+                                         magnifierWidth = 150,
+                                         zoomLevel = 1.5,
+                                         style,
+                                         alt,
+                                         onLoad,
+                                         className
+                                       }
+) {
+  const [[x, y], setXY] = useState([0, 0]);
+  const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
+  const [showMagnifier, setShowMagnifier] = useState(false);
+  return (
+    <div
+      style={{
+        position: "relative",
+      }}
+    >
+      <img
+        src={src}
+        style={style}
+        onMouseEnter={(e) => {
+          // update image size and turn-on magnifier
+          const elem = e.currentTarget;
+          const {width, height} = elem.getBoundingClientRect();
+          setSize([width, height]);
+          setShowMagnifier(true);
+        }}
+        onMouseMove={(e) => {
+          // update cursor position
+          const elem = e.currentTarget;
+          const {top, left} = elem.getBoundingClientRect();
+
+          // calculate cursor position on the image
+          const x = e.pageX - left - window.pageXOffset;
+          const y = e.pageY - top - window.pageYOffset;
+          setXY([x, y]);
+        }}
+        onMouseLeave={() => {
+          // close magnifier
+          setShowMagnifier(false);
+        }}
+        alt={alt}
+        onLoad={onLoad}
+        className={className}
+      />
+
+      <div
+        style={{
+          display: showMagnifier ? "" : "none",
+          position: "absolute",
+
+          // prevent magnifier blocks the mousemove event of img
+          pointerEvents: "none",
+          // set size of magnifier
+          height: `${magnifierHeight}px`,
+          width: `${magnifierWidth}px`,
+          // move element center to cursor pos
+          top: `${y - magnifierHeight / 2}px`,
+          left: `${x - magnifierWidth / 2}px`,
+          opacity: "1", // reduce opacity so you can verify position
+          border: "1px solid lightgray",
+          overflow: 'hidden'
+        }}
+      >
+        <img
+          src={src}
+          style={{
+            position: 'absolute',
+            ...(style || {}),
+            left: `${-x * zoomLevel + magnifierWidth / 2}px`,
+            top: `${-y * zoomLevel + magnifierHeight / 2}px`,
+            width: `${imgWidth * zoomLevel}px`,
+            height: `${imgHeight * zoomLevel}px`
+          }}
+          alt="zoom"
+        />
+      </div>
+    </div>
+  );
+}
